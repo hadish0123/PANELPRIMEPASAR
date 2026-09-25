945b36e2793816f2c71b005ec7e6b7b0be7d6c17
hadish0123 <hatf9073@gmail.com>
2026-09-25 18:34:47 -0400

    feat: register admin Telegram router

--- src/panelprimepasar/routers/__init__.py
+++ src/panelprimepasar/routers/__init__.py
@@ -1,3 +1,4 @@
+from panelprimepasar.routers.admin import router as admin_router
 from panelprimepasar.routers.customer import router as customer_router
 
-__all__ = ["customer_router"]
+__all__ = ["admin_router", "customer_router"]
