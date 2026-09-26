6f2ce07d7a679a60621a3ee970898d62f5e5c8f7
hadish0123 <hatf9073@gmail.com>
2026-09-25 22:39:25 -0400

    chore: initialize security package

---
+++ src/panelprimepasar/security/__init__.py
@@ -0,0 +1,8 @@
+from panelprimepasar.security.permissions import (
+    AdminRole,
+    Permission,
+    ROLE_PERMISSIONS,
+    has_permission,
+)
+
+__all__ = ["AdminRole", "Permission", "ROLE_PERMISSIONS", "has_permission"]
