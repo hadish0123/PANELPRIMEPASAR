6c29177332c96d8c3df379b18ce954a509dcedfb
hadish0123 <hatf9073@gmail.com>
2026-09-26 01:21:14 -0400

    feat: export web admin security helpers

--- src/panelprimepasar/security/__init__.py
+++ src/panelprimepasar/security/__init__.py
@@ -4,5 +4,24 @@ from panelprimepasar.security.permissions import (
     Permission,
     has_permission,
 )
+from panelprimepasar.security.web_admin import (
+    WebAdminSecurityError,
+    WebAdminSessionClaims,
+    create_session_token,
+    hash_password,
+    verify_password,
+    verify_session_token,
+)
 
-__all__ = ["AdminRole", "Permission", "ROLE_PERMISSIONS", "has_permission"]
+__all__ = [
+    "AdminRole",
+    "Permission",
+    "ROLE_PERMISSIONS",
+    "WebAdminSecurityError",
+    "WebAdminSessionClaims",
+    "create_session_token",
+    "hash_password",
+    "has_permission",
+    "verify_password",
+    "verify_session_token",
+]
