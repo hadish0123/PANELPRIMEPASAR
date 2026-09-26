e8ccda471e6fa6888a68aa554a3de88d823fe734
hadish0123 <hatf9073@gmail.com>
2026-09-25 22:53:30 -0400

    feat: register customer support router

--- src/panelprimepasar/routers/__init__.py
+++ src/panelprimepasar/routers/__init__.py
@@ -2,10 +2,12 @@ from panelprimepasar.routers.admin import router as admin_router
 from panelprimepasar.routers.admin_ops import router as admin_ops_router
 from panelprimepasar.routers.customer import router as customer_router
 from panelprimepasar.routers.services import router as services_router
+from panelprimepasar.routers.support import router as support_router
 
 __all__ = [
     "admin_ops_router",
     "admin_router",
     "customer_router",
     "services_router",
+    "support_router",
 ]
