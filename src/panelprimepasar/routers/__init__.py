49740787dabe6c26aae83a8237e4a99d64f3403e
hadish0123 <hatf9073@gmail.com>
2026-09-25 22:48:56 -0400

    feat: register full admin operations router

--- src/panelprimepasar/routers/__init__.py
+++ src/panelprimepasar/routers/__init__.py
@@ -1,5 +1,11 @@
 from panelprimepasar.routers.admin import router as admin_router
+from panelprimepasar.routers.admin_ops import router as admin_ops_router
 from panelprimepasar.routers.customer import router as customer_router
 from panelprimepasar.routers.services import router as services_router
 
-__all__ = ["admin_router", "customer_router", "services_router"]
+__all__ = [
+    "admin_ops_router",
+    "admin_router",
+    "customer_router",
+    "services_router",
+]
