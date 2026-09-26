37d666a14940d9727368b6c2cea5e8986bdf4e46
hadish0123 <hatf9073@gmail.com>
2026-09-26 01:39:06 -0400

    fix: export wallet router consistently

--- src/panelprimepasar/routers/__init__.py
+++ src/panelprimepasar/routers/__init__.py
@@ -3,8 +3,8 @@ from panelprimepasar.routers.admin_ops import router as admin_ops_router
 from panelprimepasar.routers.customer import router as customer_router
 from panelprimepasar.routers.discounts import router as discounts_router
 from panelprimepasar.routers.services import router as services_router
-from panelprimepasar.routers.wallet import router as wallet_router
 from panelprimepasar.routers.support import router as support_router
+from panelprimepasar.routers.wallet import router as wallet_router
 
 __all__ = [
     "admin_ops_router",
@@ -13,4 +13,5 @@ __all__ = [
     "discounts_router",
     "services_router",
     "support_router",
+    "wallet_router",
 ]
