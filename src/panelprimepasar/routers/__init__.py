e9a01711a643b222beec533e16d22bed7d18ceb5
hadish0123 <hatf9073@gmail.com>
2026-09-26 02:32:33 -0400

    feat: register payment gateway router

--- src/panelprimepasar/routers/__init__.py
+++ src/panelprimepasar/routers/__init__.py
@@ -2,6 +2,7 @@ from panelprimepasar.routers.admin import router as admin_router
 from panelprimepasar.routers.admin_ops import router as admin_ops_router
 from panelprimepasar.routers.customer import router as customer_router
 from panelprimepasar.routers.discounts import router as discounts_router
+from panelprimepasar.routers.payments import router as payments_router
 from panelprimepasar.routers.services import router as services_router
 from panelprimepasar.routers.support import router as support_router
 from panelprimepasar.routers.wallet import router as wallet_router
@@ -11,6 +12,7 @@ __all__ = [
     "admin_router",
     "customer_router",
     "discounts_router",
+    "payments_router",
     "services_router",
     "support_router",
     "wallet_router",
