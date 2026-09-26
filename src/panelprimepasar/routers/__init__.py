13ff50d28124965de2202d4273b17ba1bd94db8c
hadish0123 <hatf9073@gmail.com>
2026-09-26 01:36:05 -0400

    feat: register customer discount router

--- src/panelprimepasar/routers/__init__.py
+++ src/panelprimepasar/routers/__init__.py
@@ -1,6 +1,7 @@
 from panelprimepasar.routers.admin import router as admin_router
 from panelprimepasar.routers.admin_ops import router as admin_ops_router
 from panelprimepasar.routers.customer import router as customer_router
+from panelprimepasar.routers.discounts import router as discounts_router
 from panelprimepasar.routers.services import router as services_router
 from panelprimepasar.routers.wallet import router as wallet_router
 from panelprimepasar.routers.support import router as support_router
@@ -9,6 +10,7 @@ __all__ = [
     "admin_ops_router",
     "admin_router",
     "customer_router",
+    "discounts_router",
     "services_router",
     "support_router",
 ]
